71434a2456d23e8c75c30bd179c5fca60f0bc277
Boba Fat <ivan@workisfun.ru>
2009-04-24 03:43:07 +0400

    Revert "changed order_by_ids" it is better but slower
    This reverts commit 0e0396f9a418f67c95b03db53b4677b18f7658c9.

--- lib/active_record/acts/list.rb
+++ lib/active_record/acts/list.rb
@@ -49,13 +49,10 @@ module ActiveRecord
 
       module SingletonMethods
         def order_by_ids(ids)
+          first = find(ids.first)
           transaction do
             ids.each_with_index do |id, i|
-              position = i + 1
-              item = find(id)
-              unless item[position_column] == position
-                item.insert_at(position)
-              end
+              listed_with(first).update(id, {position_column => i + 1})
             end
           end
         end

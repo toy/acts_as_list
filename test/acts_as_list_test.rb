9f66c8ca2c6da91e2179a1a6dfea48dc92985265
Gonçalo Silva <goncalossilva@gmail.com>
2010-04-12 17:14:26 +0100

    Reorganized code. Fixed a small issue while requiring test_helper.

--- test/acts_as_list_test.rb
+++ test/acts_as_list_test.rb
@@ -1,48 +1,4 @@
-# Load the plugin's test_helper (Rails 2.x needs the path)
-begin
-  require File.dirname(__FILE__) + '/test_helper.rb'
-rescue LoadError
-  require 'test_helper'
-end
-
-def setup_db
-  ActiveRecord::Schema.define(:version => 1) do
-    create_table :mixins do |t|
-      t.column :pos, :integer
-      t.column :parent_id, :integer
-      t.column :created_at, :datetime      
-      t.column :updated_at, :datetime
-    end
-  end
-end
-
-def teardown_db
-  ActiveRecord::Base.connection.tables.each do |table|
-    ActiveRecord::Base.connection.drop_table(table)
-  end
-end
-
-class Mixin < ActiveRecord::Base
-end
-
-class ListMixin < Mixin
-  acts_as_list :column => "pos", :scope => :parent
-
-  def self.table_name() "mixins" end
-end
-
-class ListMixinSub1 < ListMixin
-end
-
-class ListMixinSub2 < ListMixin
-end
-
-class ListWithStringScopeMixin < ActiveRecord::Base
-  acts_as_list :column => "pos", :scope => 'parent_id = #{parent_id}'
-
-  def self.table_name() "mixins" end
-end
-
+require 'test_helper'
 
 class ListTest < Test::Unit::TestCase
 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,3 +6,41 @@ $:.unshift "#{File.dirname(__FILE__)}/../lib/"
 require 'acts_as_list'
 
 ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => ":memory:")
+
+def setup_db
+  ActiveRecord::Schema.define(:version => 1) do
+    create_table :mixins do |t|
+      t.column :pos, :integer
+      t.column :parent_id, :integer
+      t.column :created_at, :datetime      
+      t.column :updated_at, :datetime
+    end
+  end
+end
+
+def teardown_db
+  ActiveRecord::Base.connection.tables.each do |table|
+    ActiveRecord::Base.connection.drop_table(table)
+  end
+end
+
+class Mixin < ActiveRecord::Base
+end
+
+class ListMixin < Mixin
+  acts_as_list :column => "pos", :scope => :parent
+
+  def self.table_name() "mixins" end
+end
+
+class ListMixinSub1 < ListMixin
+end
+
+class ListMixinSub2 < ListMixin
+end
+
+class ListWithStringScopeMixin < ActiveRecord::Base
+  acts_as_list :column => "pos", :scope => 'parent_id = #{parent_id}'
+
+  def self.table_name() "mixins" end
+end
